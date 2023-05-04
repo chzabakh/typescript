c64c4eb69a042d1b569a68ec8e15fcbb0ff1fe63
Charaf eddine Zabakh <chzabakh@e2r3p4.1337.ma>
2023-05-04 23:58:46 +0100

    cleaned

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import './styles.css'
 
-const InputField = () => {
+interface Props {
+    todo: string;
+    setTodo:React.Dispatch<React.SetStateAction<string>>;
+}
+const InputField: React.FC<Props> = ({ todo, setTodo }: Props) => {
     return (
         <form className='input'>
             <input type="input" placeholder='Enter a task' className='input__box'></input>
-            <button className='input__submit'>Go</button>
+            <button className='input__submit' type="submit">Go</button>
         </form>
     )
 }
